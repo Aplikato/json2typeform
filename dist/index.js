30dc7b93fdbf0386c085c770c82e0461f834e8cb
Dirk Hornung <dirkhornung91@gmail.com>
2020-04-18 20:30:51 +0200

    Add script to run dev

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,4 @@
+"use strict";
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -7,6 +8,7 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
+Object.defineProperty(exports, "__esModule", { value: true });
 var FormType;
 (function (FormType) {
     FormType["String"] = "STRING";
@@ -272,4 +274,5 @@ class FormBuilder {
             this.footer_.appendChild(newEl);
     }
 }
-export default FormBuilder;
+exports.default = FormBuilder;
+//# sourceMappingURL=index.js.map
---
+++ runDev.sh
@@ -0,0 +1,2 @@
+./node_modules/http-server/bin/http-server -p 8080 -d false&  \ 
+tsc -w --p ./tsconfig.test.json
